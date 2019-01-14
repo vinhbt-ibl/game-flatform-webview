f16dcf4eb8d5663ba578f1681e9e9e98bce340c4
vinhbt <vinhbt@cardano-labo.com>
2019-01-14 14:10:23 +0700

    test

---
+++ precache-manifest.c644a6e5a485af9bf1c789f708be833c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ba910f091c9a74c052c0",
+    "url": "/static/js/main.ba910f09.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "ba910f091c9a74c052c0",
+    "url": "/static/css/main.7c3967f4.chunk.css"
+  },
+  {
+    "revision": "5db62253e6fbbfe0f282c7ad62543525",
+    "url": "/index.html"
+  }
+];
